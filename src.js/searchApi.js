3fff027dae7b58f7cd20d85180f1fa6630c85e5c
David Espinosa <davesespinosa@gmail.com>
2020-07-16 19:02:19 +0200

    search left

--- src.js/searchApi.js
+++ src.js/searchApi.js
@@ -6,6 +6,9 @@ const NUMBER_OF_POSES = 1;
 //variable sin valor inicial, la cual lleno con los datos de la api, me devuelve una array(objeto)
 let allPoses = null;
 
+//Variable para coger la array y meterla a lowercase
+let lowerArr = undefined;
+
 //Sería mejor traerse los datos del servidor, y luego iterar.
 async function getYogaApi() {
   //en el caso de querer un botón, vaciar la section before
@@ -14,8 +17,9 @@ async function getYogaApi() {
   try {
     const response = await fetch(`https://yoga-app-m1.herokuapp.com/yoga`);
     const data = await response.json();
-    
     allPoses = data;
+    lowerArr = data;
+    console.log(lowerArr);
     console.log(allPoses);
     const randomPoses = getRandomPoses(data);
     console.log(randomPoses);
@@ -24,24 +28,37 @@ async function getYogaApi() {
     });
   } catch (err) {}
 }
+function searchForm(input) {
+  input.addEventListener("input", function (e) {
+    var yoga = input.value;
+    console.log(yoga);
+  })
+  
+}
+searchForm(document.querySelector("#searcher"));
+
+
+//Array to string, to do lowercase, for the search
+/* function toLowerCase() {
+    const 
+}
+ */
 
 //NUeva función para añadir al buscador.
 function searchPoses(event) {
-  const serchText=  event.currentTarget.value;
+  const serchText = event.currentTarget.value;
 }
 
 function bringPose(section, data) {
-  section.innerHTML='';
+  section.innerHTML = "";
   const article = document.createElement("article");
   article.innerHTML = `
-
- <img src="${data.img_url}" alt="Keep trying little padawan"/>
+      <img src="${data.img_url}" alt="Keep trying little padawan"/>
       <p>${data.english_name}</p>
       <p>${data.sanskrit_name}<p>
     `;
   //not sure if is list or section
   section.appendChild(article);
-  console.log(data);
 }
 
 function getRandomPoses(dataArr) {
@@ -54,5 +71,3 @@ function getRandomPoses(dataArr) {
   }
   return newArr;
 }
-
-
