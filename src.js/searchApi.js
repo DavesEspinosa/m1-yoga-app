835c48818c702140a3b84e6f6bc8e2fa5d389412
David Espinosa <davesespinosa@gmail.com>
2020-07-16 20:57:11 +0200

    search done

--- src.js/searchApi.js
+++ src.js/searchApi.js
@@ -1,73 +1,63 @@
 "use strict";
 
-//Variable constante, que no va a cambiar.
-const NUMBER_OF_POSES = 1;
+const listContainer = document.querySelector("#list-container");
+const searchInput = document.querySelector("#searcher");
+let data = [];
+let input = "";
 
-//variable sin valor inicial, la cual lleno con los datos de la api, me devuelve una array(objeto)
-let allPoses = null;
-
-//Variable para coger la array y meterla a lowercase
-let lowerArr = undefined;
-
-//Sería mejor traerse los datos del servidor, y luego iterar.
-async function getYogaApi() {
-  //en el caso de querer un botón, vaciar la section before
-  const section = document.querySelector(".advice");
-
+async function getData() {
   try {
     const response = await fetch(`https://yoga-app-m1.herokuapp.com/yoga`);
     const data = await response.json();
-    allPoses = data;
-    lowerArr = data;
-    console.log(lowerArr);
-    console.log(allPoses);
-    const randomPoses = getRandomPoses(data);
-    console.log(randomPoses);
-    randomPoses.forEach((element) => {
-      bringPose(section, element);
-    });
-  } catch (err) {}
-}
-function searchForm(input) {
-  input.addEventListener("input", function (e) {
-    var yoga = input.value;
-    console.log(yoga);
-  })
-  
-}
-searchForm(document.querySelector("#searcher"));
-
-
-//Array to string, to do lowercase, for the search
-/* function toLowerCase() {
-    const 
-}
- */
-
-//NUeva función para añadir al buscador.
-function searchPoses(event) {
-  const serchText = event.currentTarget.value;
-}
-
-function bringPose(section, data) {
-  section.innerHTML = "";
-  const article = document.createElement("article");
-  article.innerHTML = `
-      <img src="${data.img_url}" alt="Keep trying little padawan"/>
-      <p>${data.english_name}</p>
-      <p>${data.sanskrit_name}<p>
-    `;
-  //not sure if is list or section
-  section.appendChild(article);
-}
-
-function getRandomPoses(dataArr) {
-  const newArr = [];
-  for (let i = 0; i < NUMBER_OF_POSES; i++) {
-    let randomId = Math.floor(Math.random() * dataArr.length);
-    let pose = dataArr.splice(randomId, 1)[0];
-
-    newArr.push(pose);
+    return data;
+  } catch (err) {
+    console.error("ERROR DOWNLOADING YOGA DATA", err);
+    return [];
   }
-  return newArr;
 }
+
+function insertListRows(rows) {
+  rows.forEach((row) => {
+    const rowDOM = document.createElement("div");
+    rowDOM.innerHTML = row.english_name;
+    listContainer.appendChild(rowDOM);
+  });
+}
+
+function onSearch(ev) {
+  input = ev.target.value;
+  listContainer.innerHTML = "";
+
+  if (!input) {
+    return insertListRows([]);
+  }
+
+  const filteredData = data.filter(function (el) {
+    return el.english_name.toLowerCase().includes(input.toLowerCase());
+  });
+
+  insertListRows(filteredData);
+}
+
+function onEnterPress(ev) {
+  if (ev.code !== "Enter") return;
+
+  event.preventDefault();
+
+  const searchModal = document.createElement("div");
+	searchModal.classList = "modal-body";
+  const searchedElement = JSON.stringify(
+    data.find((el) =>
+      el.english_name.toLowerCase().includes(input.toLowerCase())
+    )
+  );
+  console.log(searchedElement);
+	searchModal.innerHTML = searchedElement;
+	document.body.appendChild(searchModal);
+}
+
+window.onload = async () => {
+  searchInput.addEventListener("keydown", onSearch);
+  document.addEventListener("keydown", onEnterPress);
+  data = await getData();
+};
